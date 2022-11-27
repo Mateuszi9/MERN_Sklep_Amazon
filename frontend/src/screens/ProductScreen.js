38f6118f858875e68cc2f76e4bd52e7566036539
Mateusz Izdebski <mati2k51@gmail.com>
2022-11-27 10:42:19 +0100

    13-create-loading-and-message-component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -10,6 +10,9 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Rating from './../components/Rating.js';
 import Button from 'react-bootstrap/esm/Button.js';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox.js';
+import MessageBox from '../components/MessageBox.js';
+import { getError } from '../utils.js';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -41,16 +44,16 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
 
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
