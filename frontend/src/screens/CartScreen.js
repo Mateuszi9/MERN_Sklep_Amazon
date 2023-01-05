f23616098582964d059f150108903d9d9f490a80
Mateusz Izdebski <mati2k51@gmail.com>
2023-01-05 10:27:08 +0100

    Create-shipping-screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -33,7 +33,7 @@ export default function CartScreen() {
   };
 
   const checkoutHandler = () => {
-    navigate('/signin?redirect=/shopping');
+    navigate('/signin?redirect=/shipping');
   };
 
   return (
